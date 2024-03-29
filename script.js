85ab76033fcfa3ba51c6201d43421ecd26a56f6e
prince kumar sacxena <86111082+prince-9211@users.noreply.github.com>
2024-03-28 19:20:22 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function updatePragraph(){
+	var StatusParagraph=document.getElementById("status");
+	StatusParagraph.innerHTML="<h1>Entered Metaverse</h1>";
+}
+//Adding EventListner......
+document.getElementById("enterBtn").addEventListener("click",updatePragraph);
